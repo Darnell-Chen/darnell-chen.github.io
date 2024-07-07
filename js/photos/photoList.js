f02d214b7ff830d5dba9b13fe289ffa70fd45270
Darnell-Chen <realdarnellchen@gmail.com>
2024-07-07 01:39:05 -0400

    will continue working on the photo page

--- js/photos/photoList.js
+++ js/photos/photoList.js
@@ -1,64 +1,44 @@
 const photoList = [{
     photo: "mainPicture.jpg",
-    where: "@Cattyshack Cafe, Fort Myers",
+    where: "@ Cattyshack Cafe, Fort Myers",
     date: "July 18, 2023"
 },{
     photo: "04-25-2023-1.jpg",
-    where: "@Treasure Island, CA",
+    where: "@ Treasure Island, CA",
     date: "April 25, 2023"
 },{
     photo: "04-25-2023-2.jpg",
-    where: "@Fremont, CA",
+    where: "@ Zach's Place",
     date: "April 25, 2023",
     description: "Zach's Apartment"
 },{
-    photo: "04-25-2023-3.jpg",
-    where: "@Fremont, CA",
-    date: "April 25, 2023",
-    description: "Heading Back from Zach's"
-},{
     photo: "04-25-2023-4.jpg",
-    where: "@Communication Hills, San Jose",
+    where: "@ Communication Hills, San Jose",
     date: "April 25, 2023",
     description: "The hill left thorns in my socks"
 },{
     photo: "05-02-2023-1.jpg",
-    where: "Cow Hollow, SF",
+    where: "@ Cow Hollow, SF",
     date: "May 2nd, 2023"
 },{
     photo: "05-06-2023-1.jpg",
-    where: "@Fromm Hall, SF",
+    where: "@ Fromm Hall, SF",
     date: "May 6, 2023",
     description: "Me and Bebey (the Teddy Bear)"
 },{
-    photo: "05-09-2023-1.jpg",
-    where: "@Golden Gate Bridge",
-    date: "May 9, 2023",
-    description: "Late to the Sunset at Golden Gate Bridge"
-},{
     photo: "05-18-2023-1.jpg",
-    where: "@DeYoung Museum, SF",
+    where: "@ DeYoung Museum, SF",
     date: "May 18, 2023",
     description: "With the duckies at Blue Heron Lake"
 },{
-    photo: "05-18-2023-2.jpg",
-    where: "@Golden Gate Park, SF",
-    date: "May 18, 2023",
-    description: "Big Ol Statue!!!"
-},{
     photo: "07-18-2023-1.jpg",
-    where: "@Fort Myers, FL",
+    where: "@ Fort Myers, FL",
     date: "July 18, 2023"
 },{
     photo: "07-19-2023-2.jpg",
-    where: "@Fort Myers, FL",
+    where: "@ Fort Myers, FL",
     date: "July 19, 2023",
     description: "Teddy waiting for me"
-},{
-    photo: "08-15-2023-1.jpg",
-    where: "@GLC Apt 123, Atlanta",
-    date: "October 15, 2023",
-    description: "T-Posing for Facetime"
 }
 ]
 
--- js/photos/photos.js
+++ js/photos/photos.js
@@ -1,14 +1,24 @@
 import photoList from "./photoList.js";
 
 $(document).ready(function() {
+
+
+    let listPhotos = genImageList();
+    setPhotos(listPhotos);
+
+    // we need to delete alt-intro slide after the duration of its anim
+
+})
+
+const genImageList = () => {
     const numPhotos = photoList.length;
 
     // our list that will hold which photos to get. 
     // We'll go ahead and instantiate with one num
-    const genPhotos = [Math.floor(Math.random() * numPhotos.length)];
+    const genPhotos = [Math.floor(Math.random() * numPhotos)];
 
-    while (genPhotos.length < 3) {
-        const newPhoto = Math.floor(Math.random() * numPhotos.length);
+    while (genPhotos.length < 4) {
+        const newPhoto = Math.floor(Math.random() * numPhotos);
 
         let add = true;
         for (let i = 0; i < genPhotos.length; i++) {
@@ -21,5 +31,32 @@ $(document).ready(function() {
         add ? genPhotos[genPhotos.length] = newPhoto : null;
     }
 
-    
-})
+    return genPhotos;
+}
+
+const setPhotos = (listPhotos) => {
+    // we'll create a for loop for each photo. They will each be in a col-md div. 
+
+    let replacedText = "";
+
+
+    for (let i = 0; i < listPhotos.length; i++) {
+        const currPhoto = photoList[listPhotos[i]];
+
+        let start = '<div class="photo col-md">' +
+                    '<div class="photo-polaroid-padding">';
+
+        start += '<img src="../../images/myPics/' + currPhoto.photo +'">' +
+                 '<p class="photo-text">' +
+                 '<span class="photo-left-text">' + currPhoto.where + '</span>' +
+                 '<span class="photo-right-text">' + currPhoto.date + '</span>'
+
+
+
+        start += '</p></div></div>';
+
+        replacedText +=start
+    }
+
+    $(".photo-wrapper").append(replacedText);
+}
